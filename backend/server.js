5d7076ada264eb0e018567337b86aabe9c129ca1
Haseeb Ahmed <107324392+Halmoside@users.noreply.github.com>
2025-12-07 01:53:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ import usersRouter from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+  ));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -27,3 +29,4 @@ start().catch(err => {
   console.error(err);
   process.exit(1);
 });
+
