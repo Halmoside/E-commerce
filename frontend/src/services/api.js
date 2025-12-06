52dd657835fa6033d2fa27b428439e5fd9938918
Haseeb Ahmed <107324392+Halmoside@users.noreply.github.com>
2025-12-07 02:05:49 +0530

    Update API endpoints to use production URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,19 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL ; 
+
 
 export async function fetchProducts() {
-  const res = await fetch(`${API_BASE}/api/products`);
+  const res = await fetch(`${https://e-commerce-9paa.onrender.com}/api/products`);
   if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
 }
 
 export async function fetchProductById(id) {
-  const res = await fetch(`${API_BASE}/api/products/${id}`);
+  const res = await fetch(`${https://e-commerce-9paa.onrender.com}/api/products/${id}`);
   if (!res.ok) throw new Error("Failed to fetch product");
   return res.json();
 }
 
 export async function registerUser(data) {
-  const res = await fetch(`${API_BASE}/api/users/register`, {
+  const res = await fetch(`${https://e-commerce-9paa.onrender.com}/api/users/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
@@ -22,7 +22,7 @@ export async function registerUser(data) {
 }
 
 export async function loginUser(data) {
-  const res = await fetch(`${API_BASE}/api/users/login`, {
+  const res = await fetch(`${https://e-commerce-9paa.onrender.com}/api/users/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
@@ -31,10 +31,11 @@ export async function loginUser(data) {
 }
 
 export async function getCurrentUser(token) {
-  const res = await fetch(`${API_BASE}/api/users/current`, {
+  const res = await fetch(`${https://e-commerce-9paa.onrender.com}/api/users/current`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.json();
 }
+
 
 
